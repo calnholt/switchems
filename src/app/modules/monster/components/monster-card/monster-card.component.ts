8ca9aa70e265c8154dcc7963b2455d2a06c7fc4a
Calvin <calnholt@gmail.com>
2020-02-17 17:14:05 -0500

    parse text for terms and images

--- src/app/modules/monster/components/monster-card/monster-card.component.ts
+++ src/app/modules/monster/components/monster-card/monster-card.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MonsterCardComponent implements OnInit {
   @Input() monster: MonsterComplete;
+  monsterAbilityHtml: string;
   ELEMENT_LIST: string[] = ['Fire', 'Water', 'Rock', 'Leaf', 'Electric', 'Skull'];
   ICON_PATH: string = './assets/images';
   ROLE_PATH: string = this.ICON_PATH + '/roles';
@@ -59,8 +60,67 @@ export class MonsterCardComponent implements OnInit {
     }
   };
 
-  ngOnInit() {}
-  constructor() {}
+  VARIOUS_ICON_DIRECTORY: string = './assets/images/symbols/';
+  ELEMENT_ICON_DIRECTORY: string = './assets/images/elements/color/';
+
+  TERMS_MAP: any = {
+    '~BURN~': 'Burned monsters gain -1[ATK]. Burned [L] [R] [S] monsters gain -2[ATK] instead.',
+    '~SUCCESS~': 'Unsuccessful attacks do nothing.',
+    '~FLINCH~':	'Prevent the targeted monster\'s attack if this attack is faster speed.',
+    '~PARALYZE~': 'Paralyzed monsters have -2[SPD] and all of their attacks have [2]: This attack is successful.',
+    '~LEECH~': 'At the end of each turn, deal 1[ATK] to the leeched monster. If your active monster '
+      + 'is [L], heal 1[HP]. Stacks up to three. Remove on switch.',
+    '~FATIGUE~': 'Fatigued monsters gain -1[DEF]. Fatigued [W] [E] [F] monsters can\'t use buff cards.',
+    '~PREVENT ESC~': 'Cannot be prevented with Can\'t Escape!',
+    '~STATUS~': 'Paralyze, burn, leech, etc.',
+    '~SINGLE~':	'Recharges on switch.',
+    '~CONSECUTIVE~': 'Cannot be used consecutively.',
+    '~STUN~': 'Stunned monsters cannot switch.',
+    '~RECOIL~': 'Recoil damage cannot be prevented and still occurs if the targeted monster protects.',
+    '~CURSE~': 'If this card is flipped for a [X] effect, that monster is dealt 1[ATK].'
+  };
+
+  IMG_MAP = {
+    '[ATK]': this.VARIOUS_ICON_DIRECTORY + 'attack.png',
+    '[PH]': this.VARIOUS_ICON_DIRECTORY + 'physical.png',
+    '[SP]': this.VARIOUS_ICON_DIRECTORY + 'special.png',
+    '[+]': this.VARIOUS_ICON_DIRECTORY + 'draw.png',
+    '[B]': this.VARIOUS_ICON_DIRECTORY + 'buff.png',
+    '[-]': this.VARIOUS_ICON_DIRECTORY + 'pay.png',
+    '[1]': this.VARIOUS_ICON_DIRECTORY + '1.png',
+    '[2]': this.VARIOUS_ICON_DIRECTORY + '2.png',
+    '[3]': this.VARIOUS_ICON_DIRECTORY + '3.png',
+    '[DEF]': this.VARIOUS_ICON_DIRECTORY + 'defense.png',
+    '[TA]': this.VARIOUS_ICON_DIRECTORY + 'aura.png',
+    '[X]': this.VARIOUS_ICON_DIRECTORY + 'x.png',
+    '[SUCC]': this.VARIOUS_ICON_DIRECTORY + 'success.png',
+    '[FAIL]': this.VARIOUS_ICON_DIRECTORY + 'fail.png',
+    '[SPD]': this.VARIOUS_ICON_DIRECTORY + 'speed.png',
+    '[F]': this.ELEMENT_ICON_DIRECTORY + 'fire.png',
+    '[W]': this.ELEMENT_ICON_DIRECTORY + 'water.png',
+    '[L]': this.ELEMENT_ICON_DIRECTORY + 'leaf.png',
+    '[R]': this.ELEMENT_ICON_DIRECTORY + 'rock.png',
+    '[E]': this.ELEMENT_ICON_DIRECTORY + 'elec.png',
+    '[S]': this.ELEMENT_ICON_DIRECTORY + 'skull.png',
+    '[ST]': this.VARIOUS_ICON_DIRECTORY + 'status.png',
+    '[REAC]': this.VARIOUS_ICON_DIRECTORY + 'reaction.png',
+    '[HP]': this.VARIOUS_ICON_DIRECTORY + 'heart.png',
+  };
+
+  TERMS_ARRAY: string[] = [];
+  IMG_ARRAY: string[] = [];
+
+  TERM_CSS: string = 'term';
+  ABILITY_IMG_CSS: string = 'term-img';
+
+  ngOnInit() {
+    this.monsterAbilityHtml = this.monster.abilityText;
+  }
+
+  constructor() {
+    Object.keys(this.TERMS_MAP).forEach(term => this.TERMS_ARRAY.push(term));
+    Object.keys(this.IMG_MAP).forEach(img => this.IMG_ARRAY.push(img));
+  }
 
   getEffectivenessArray(monster: MonsterComplete) {
     const arrs = [].concat(monster.elementLks.map((el: string) => this.ELEMENT_MAP[el].advantages));
@@ -96,6 +156,23 @@ export class MonsterCardComponent implements OnInit {
 
   getHpIcon() {
     return `${this.ICON_PATH}/symbols/hp/${this.monster.hp}.png`;
+  }
+
+  getAbilityText() {
+    let innerHtml = this.monster.abilityText;
+    this.TERMS_ARRAY.forEach((term: string) => {
+      while (innerHtml.includes(term)) {
+          const html = `<br><span class="${this.TERM_CSS}">(${this.TERMS_MAP[term]})</span>`;
+          innerHtml = innerHtml.replace(term, html);
+      }
+    });
+    this.IMG_ARRAY.forEach((img: string) => {
+      while (innerHtml.includes(img)) {
+          const html = `<img src="${this.IMG_MAP[img]}" class="${this.ABILITY_IMG_CSS}">`;
+          innerHtml = innerHtml.replace(img, html);
+      }
+    });
+    return innerHtml;
   }
 
 }
--- src/app/modules/monster/components/monster-form/monster-form.component.ts
+++ src/app/modules/monster/components/monster-form/monster-form.component.ts
@@ -17,63 +17,9 @@ export class MonsterFormComponent implements OnInit {
   elements: FormControl;
   elementList: string[] = ['Fire', 'Water', 'Rock', 'Leaf', 'Electric', 'Skull'];
   roleList: string[] = ['Warrior', 'Assassin', 'Technical', 'Tank', 'Support', 'Tricky'];
-  VARIOUS_ICON_DIRECTORY: string = './assets/images/symbols/';
-  ELEMENT_ICON_DIRECTORY: string = './assets/images/elements/color/';
-
-  TERMS_MAP: any = {
-    '~BURN~': 'Burned monsters gain -1[ATK]. Burned [L] [R] [S] monsters gain -2[ATK] instead.',
-    '~SUCCESS~': 'Unsuccessful attacks do nothing.',
-    '~FLINCH~':	'Prevent the targeted monster\'s attack if this attack is faster speed.',
-    '~PARALYZE~': 'Paralyzed monsters have -2[SPD] and all of their attacks have [2]: This attack is successful.',
-    '~LEECH~': 'At the end of each turn, deal 1[ATK] to the leeched monster. If your active monster '
-      + 'is [L], heal 1[HP]. Stacks up to three. Remove on switch.',
-    '~FATIGUE~': 'Fatigued monsters gain -1[DEF]. Fatigued [W] [E] [F] monsters can\'t use buff cards.',
-    '~PREVENT ESC~': 'Cannot be prevented with Can\'t Escape!',
-    '~STATUS~': 'Paralyze, burn, leech, etc.',
-    '~SINGLE~':	'Recharges on switch.',
-    '~CONSECUTIVE~': 'Cannot be used consecutively.',
-    '~STUN~': 'Stunned monsters cannot switch.',
-    '~RECOIL~': 'Recoil damage cannot be prevented and still occurs if the targeted monster protects.',
-    '~CURSE~': 'If this card is flipped for a [X] effect, that monster is dealt 1[ATK].'
-  };
-
-  IMG_MAP = {
-    '[ATK]': this.VARIOUS_ICON_DIRECTORY + 'attack.png',
-    '[PH]': this.VARIOUS_ICON_DIRECTORY + 'physical.png',
-    '[SP]': this.VARIOUS_ICON_DIRECTORY + 'special.png',
-    '[+]': this.VARIOUS_ICON_DIRECTORY + 'draw.png',
-    '[B]': this.VARIOUS_ICON_DIRECTORY + 'buff.png',
-    '[-]': this.VARIOUS_ICON_DIRECTORY + 'pay.png',
-    '[1]': this.VARIOUS_ICON_DIRECTORY + '1.png',
-    '[2]': this.VARIOUS_ICON_DIRECTORY + '2.png',
-    '[3]': this.VARIOUS_ICON_DIRECTORY + '3.png',
-    '[DEF]': this.VARIOUS_ICON_DIRECTORY + 'defense.png',
-    '[TA]': this.VARIOUS_ICON_DIRECTORY + 'aura.png',
-    '[X]': this.VARIOUS_ICON_DIRECTORY + 'x.png',
-    '[SUCC]': this.VARIOUS_ICON_DIRECTORY + 'success.png',
-    '[FAIL]': this.VARIOUS_ICON_DIRECTORY + 'fail.png',
-    '[SPD]': this.VARIOUS_ICON_DIRECTORY + 'speed.png',
-    '[F]': this.ELEMENT_ICON_DIRECTORY + 'fire.png',
-    '[W]': this.ELEMENT_ICON_DIRECTORY + 'water.png',
-    '[L]': this.ELEMENT_ICON_DIRECTORY + 'leaf.png',
-    '[R]': this.ELEMENT_ICON_DIRECTORY + 'rock.png',
-    '[E]': this.ELEMENT_ICON_DIRECTORY + 'elec.png',
-    '[S]': this.ELEMENT_ICON_DIRECTORY + 'skull.png',
-    '[ST]': this.VARIOUS_ICON_DIRECTORY + 'status.png',
-    '[REAC]': this.VARIOUS_ICON_DIRECTORY + 'reaction.png',
-    '[HP]': this.VARIOUS_ICON_DIRECTORY + 'heart.png',
-  };
-
-  TERMS_ARRAY: string[] = [];
-  IMG_ARRAY: string[] = [];
-
-  TERM_CSS: string = 'term';
-  ABILITY_IMG_CSS: string = 'term-img';
 
   constructor() {
     this.populateMonster();
-    Object.keys(this.TERMS_MAP).forEach(term => this.TERMS_ARRAY.push(term));
-    Object.keys(this.IMG_MAP).forEach(img => this.IMG_ARRAY.push(img));
   }
 
   ngOnInit() {}
@@ -103,22 +49,4 @@ export class MonsterFormComponent implements OnInit {
       this.elements.setValue(this.monster.elementLks);
     }
   }
-
-  // will need to abstract this for all cards
-  convertText(text: string) {
-    this.TERMS_ARRAY.forEach((term: string) => {
-      while (text.includes(term)){
-          const html = `<br><span class="${this.TERM_CSS}?>(${this.TERMS_MAP[term]})</span>`;
-          text = text.replace(term, html);
-      }
-    });
-    this.IMG_ARRAY.forEach((img: string) => {
-      while (text.includes(img)){
-          const html = `<img src="${this.IMG_MAP[img]}" class="${this.ABILITY_IMG_CSS}">`;
-          text = text.replace(img, html);
-      }
-    });
-    this.monster.abilityText = text;
-  }
-
 }
