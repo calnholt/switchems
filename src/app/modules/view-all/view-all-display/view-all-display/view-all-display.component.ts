d2614c32dcccaa925d7e5d27d38ea46e2af7e7a3
Calvin <calnholt@gmail.com>
2020-02-26 07:47:29 -0500

    cleanup

--- src/app/modules/view-all/view-all-display/view-all-display/view-all-display.component.ts
+++ src/app/modules/view-all/view-all-display/view-all-display/view-all-display.component.ts
@@ -3,14 +3,6 @@ import { loadMonsters } from './../../../import/json-to-obj';
 import { MonsterComplete } from 'src/app/modules/monster/model/monster';
 import { Component, OnInit } from '@angular/core';
 
-class SelectedCard {
-  monsterId?: number;
-  buff?: Buff;
-  action?: Action;
-  index?: number;
-  isLocked?: boolean;
-}
-
 @Component({
   selector: 'view-all-display',
   templateUrl: './view-all-display.component.html',
@@ -19,49 +11,14 @@ class SelectedCard {
 
 export class ViewAllDisplayComponent implements OnInit {
   monsters: Array<MonsterComplete>;
-  selectedCard: SelectedCard;
   constructor() { }
 
   ngOnInit() {
     this.monsters = loadMonsters();
-    this.selectedCard = new SelectedCard();
-    this.selectedCard.monsterId = null;
-    this.selectedCard.action = null;
-    this.selectedCard.buff = null;
-  }
-
-  lockSelectedCard(action: Action, buff: Buff) {
-    if (action) {
-      if (this.selectedCard.isLocked) {
-        this.selectedCard.action = action;
-      }
-    }
-    this.selectedCard.isLocked = !this.selectedCard.isLocked;
   }
 
   mouseoverAction(action: Action) {
     action.isSelected = true;
-  }
-
-  mouseoverCard(monster: MonsterComplete, index: number, isAction: boolean) {
-    if (this.selectedCard.isLocked) {
-      return;
-    }
-    this.selectedCard.monsterId = monster.monsterId;
-    this.selectedCard.index = index;
-    if (isAction) {
-      this.selectedCard.action = monster.actions[index];
-    } else {
-      this.selectedCard.buff = monster.buffs[index];
-    }
-  }
-
-  mouseoutCard() {
-    if (!this.selectedCard.isLocked) {
-      this.selectedCard.action = null;
-      this.selectedCard.buff = null;
-      this.selectedCard.monsterId = null;
-    }
   }
 
   isShowMonster(monster: MonsterComplete) {
