f3e1ae3741041090195d8ff68550b993d685e9c8
Calvin Holt <calnholt@gmail.com>
2020-10-30 08:42:23 -0400

    format for print; 4 players worth

--- src/app/modules/print/pnp/pnp.component.ts
+++ src/app/modules/print/pnp/pnp.component.ts
@@ -57,7 +57,9 @@ export class PnpComponent implements OnInit {
         allCards.push(b);
       });
       // adds player boards
-      PLAYER_BOARD_TEXT.forEach(txt => allCards.push({isPlayerBoard: true, text: txt}));
+      for (let i = 0; i < 4; i++) {
+        PLAYER_BOARD_TEXT.forEach(txt => allCards.push({isPlayerBoard: true, text: txt}));
+      }
     }
     this.allCards = allCards;
     this.count = 0;
