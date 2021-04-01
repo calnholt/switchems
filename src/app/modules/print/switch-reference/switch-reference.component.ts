821aacd232c12bb0372bd338084301add73b157f
Calvin Holt <calnholt@gmail.com>
2021-04-01 07:28:26 -0400

    update for resistant icon

--- src/app/modules/print/switch-reference/switch-reference.component.ts
+++ src/app/modules/print/switch-reference/switch-reference.component.ts
@@ -16,7 +16,7 @@ export class SwitchReferenceComponent implements OnInit {
       'Heal 2[HP] OR remove a [STATUS] from the monster that is switching out',
       'Remove all [NQ] from your Stat Cube Board',
       'Remove all but two [PQ] from your Stat Cube Board (all [DEF][PQ] stacks count as one [PQ])',
-      'The monster switching in gains +X[DEF] this turn against attacks with elements it is resistant to (as denoted on the bottom right of the monster card)'
+      'The monster switching in gains +X[DEF] this turn against attacks with elements it is <b>resistant</b> [RESIST] to'
     ];
 
   constructor() { }
